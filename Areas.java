13ff72d7c2c8609adfa9b548f9404767c5d0eadc
siamahajani9 <83707461+siamahajani9@users.noreply.github.com>
2021-05-09 11:47:50 +0530

    Calculates areas of all shapes.

---
+++ Areas.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    int a = 2;
+    int b = 2; 
+    System.out.println("area of a square is:");
+    System.out.println(a*b);
+    double c = 3.14;
+    int r = 5;
+    System.out.println("area of circle is:");
+    System.out.println(c*r*r);
+    double g = 0.5;
+    int ba = 3;
+    int h = 4;
+     System.out.println("area of a triangle is:");
+     System.out.println(g*ba*h);
+     int l = 5;
+     int br = 6;
+     System.out.println("area of a rectangle is;");
+     System.out.println(l*br);
+  }                                                                                   
+}
